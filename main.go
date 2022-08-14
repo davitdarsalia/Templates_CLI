839309a5b575966bb4e7b202a8cd721d9a26a7b2
davitdarsalia <darsalia.david1998@gmail.com>
2022-08-14 14:14:11 +0400

    feat: RootCommand Executor Defined + Logrus Config

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/davitdarsalia/CLI_Commands/reactNativeGen"
+	"github.com/davitdarsalia/CLI_Commands/cmd"
+	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
-	reactNativeGen.GenerateRNTemplate("Input")
+	//reactNativeGen.GenerateRNTemplate("CalendarPicker")
+	cmd.ExecuteRootCMD()
+}
+
+func init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{
+		PrettyPrint: true,
+	})
+
+	logrus.SetOutput(os.Stderr)
+	logrus.SetLevel(logrus.WarnLevel)
 }
