63db7230697dce03af1e1ad614d8a513571d1bf8
Davit Darsalia <darsalia.david1998@gmail.com>
2022-08-28 02:47:05 +0400

    Update mainScripts.bash

--- mainScripts.bash
+++ mainScripts.bash
@@ -1,3 +1,5 @@
 go run main.go rnGen template --template=Ts
 go run main.go rnGen template --template=Js
 go build -o file.exe file.go
+
+./main rnGen --Template=Ts --C=DatePicker --SubExtension="Screen"
