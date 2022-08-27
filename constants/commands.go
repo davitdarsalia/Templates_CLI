235b767e81d17b170d28642110c70643a4a80151
davitdarsalia <darsalia.david1998@gmail.com>
2022-08-28 00:34:17 +0400

    feat:  Command Constants Additions

--- constants/commands.go
+++ constants/commands.go
@@ -5,8 +5,11 @@ const (
 	Template          = "template"
 	TemplateShortHand = "t"
 	TemplateFlag      = "template"
+	C                 = "c"
+	CShorthand        = "c"
 )
 
 const (
 	TemplateDescription = "Choose Template Extension"
+	CDescription        = "Choose A Name For A Component"
 )
